4d439c884f7f93a3915158ee983887bcb56e3283
ss223ck <ss223ck@student.lnu.se>
2017-03-09 13:45:16 +0100

    Created project in visual studio

---
+++ asp.net-core-REST-application/src/asp.net-core-REST-application/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace asp.net_core_REST_application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
