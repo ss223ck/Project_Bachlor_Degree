0a2b207f17fa2ea28057030a66e25c482ce5799c
ss223ck <ss223ck@student.lnu.se>
2017-04-04 13:04:40 +0200

    Full rest implemented

--- net-core-REST-web-api/src/net-core-REST-web-api/Controllers/ValuesController.cs
+++ net-core-REST-web-api/src/net-core-REST-web-api/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using net_core_REST_web_api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace net_core_REST_web_api.Controllers
 {
@@ -21,33 +22,89 @@ namespace net_core_REST_web_api.Controllers
         [HttpGet]
         public IEnumerable<Person> Get()
         {
-            var list = _context.Person.ToList();
+            var list = _context.Person.Take(20);
             return list;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult Get(int id)
         {
-            return "value";
+            try
+            {
+                Person person = _context.Person.Find(id);
+                if (person != null)
+                {
+                    return Ok(person);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public ActionResult Post([FromBody]Person person)
         {
+            try
+            {
+                _context.Person.Add(person);
+                _context.SaveChanges();
+                return Created("", person);
+            } catch
+            {
+                return StatusCode(500);
+            }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public ActionResult Put([FromBody]Person person)
         {
+            try
+            {
+                if (_context.Entry(person).State == EntityState.Detached)
+                {
+                    _context.Person.Attach(person);
+                }
+                _context.Entry(person).State = EntityState.Modified;
+                _context.SaveChanges();
+                return Ok(person);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            try
+            {
+                Person person = _context.Person.Find(id);
+                if (person != null)
+                {
+                    _context.Person.Remove(person);
+                    _context.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500); 
+            }
         }
     }
 }
